babc23fd9cd0f5759f8e2c4851123a36b4be3365
Ömer Çağrı Bozdemir <cagribozdemir0@gmail.com>
2022-10-11 12:45:35 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyDictionary<TKey, TValue> // Generic Class
+    {
+        KeyValuePair<TKey, TValue>[] _array;
+        KeyValuePair<TKey, TValue>[] _tempArray;
+        public MyDictionary()
+        {
+            _array = new KeyValuePair<TKey, TValue>[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            _tempArray = _array;
+            _array = new KeyValuePair<TKey, TValue>[_array.Length+1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+
+            _array[_array.Length-1]= new KeyValuePair<TKey, TValue>(key, value);
+        }
+
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cities = new Dictionary<string, string>();
+
+            cities.Add("UK", "London, Manchester, Birmingham");
+            cities.Add("USA", "New York, Chicago, Washington");
+            cities.Add("Turkey", "İstanbul, Ankara, İzmir");
+
+            Console.WriteLine(cities.Count);
+            Console.WriteLine(cities["USA"] + "/" + cities["UK"] + "/" + cities["Turkey"]);
+
+            MyDictionary<string, string> cities2 = new MyDictionary<string, string>();
+
+            cities2.Add("UK", "London, Manchester, Birmingham");
+            cities2.Add("USA", "New York, Chicago, Washington");
+            cities2.Add("Turkey", "İstanbul, Ankara, İzmir");
+
+            Console.WriteLine(cities2.Count);
+            Console.WriteLine(cities["USA"] + "/" + cities["UK"] + "/" + cities["Turkey"]);
+        }
+    }
+}
